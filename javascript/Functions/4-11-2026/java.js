9b018b3b2c8b1e59d56f9cb220b85eea1507d23a
qazimoizz <qazimoiz222@gmail.com>
2026-04-12 17:36:50 +0500

    solved some practice questions

--- javascript/Functions/4-11-2026/java.js
+++ javascript/Functions/4-11-2026/java.js
@@ -1,11 +1,64 @@
-function countVowels(str) {
-    let count = 0;
-    for(char of str ){
-            if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u" ){
+// // let arr = [1,2,3,4,5]
 
-                count++
-        }
-    }
+// // arr.forEach((val)=>{
 
-    return count;
-} 
+// //     console.log(val * 2);
+// // })
+
+
+// let n = prompt("enter the number")
+
+// let arr = []
+
+// for( i=1; i<=n; i++ ) {
+//     arr[i-1]= i;
+
+// }
+// console.log(arr)
+
+// let sum = arr.reduce((prev, curr) =>{
+
+//     return prev + curr
+
+// })
+
+// console.log("sum =>", sum)
+
+// let factorial = arr.reduce((res , curr)=>{
+
+//     return res * curr;
+
+// })
+
+// console.log("factorial =>" , factorial)
+
+    // Practice Mode
+
+// for(let i=1; i<=20; i++){
+ 
+//     if(i % 2 === 0  ){
+    
+//     console.log(i)
+// }
+// }
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let sum = 0;
+// for(let i=1; i<=100; i++){
+//     sum += i; 
+// }
+
+// console.log(sum)
+
+let num = 123;
+// let reverse = 0;
+
+// while(num>0){
+//     let digit = num % 10;
+//     reverse = reverse * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+// console.log(reverse) 
+// console.log()
+
+console.log(num.toString().split("").reverse().join().number(num))
