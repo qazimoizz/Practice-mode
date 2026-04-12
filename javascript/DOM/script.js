7e9f851a791104667f05587c0833ef33172fac0e
qazimoizz <qazimoiz222@gmail.com>
2026-04-12 22:33:03 +0500

    last commit before closing

--- javascript/DOM/script.js
+++ javascript/DOM/script.js
@@ -3,6 +3,6 @@
 // heading.innerText = heading.innerText + " " + "apna college" 
 
 
-let divs = document.querySelectorAll(".box");
-divs[0].innertext = "apna college"; 
-
+    let divs = document.querySelectorAll(".box");
+    divs[0].innerText = "apna college"; 
+    divs[1].innerText = "uniquw text";
