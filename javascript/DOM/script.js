e8f2fbd3821943247fe64019794a098d6e82b4e1
qazimoizz <qazimoiz222@gmail.com>
2026-04-12 21:24:07 +0500

    first commit

---
+++ javascript/DOM/script.js
@@ -0,0 +1,8 @@
+// let heading = document.querySelector("h2")
+// console.dir(heading)
+// heading.innerText = heading.innerText + " " + "apna college" 
+
+
+let divs = document.querySelectorAll(".box");
+divs[0].innertext = "apna college"; 
+
