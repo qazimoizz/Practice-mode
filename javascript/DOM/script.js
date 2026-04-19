f343805582e4cb93ebcd00125c460c29dfeddfea
qazimoizz <qazimoiz222@gmail.com>
2026-04-19 19:31:02 +0500

    work done

--- javascript/DOM/script.js
+++ javascript/DOM/script.js
@@ -92,5 +92,151 @@
 //     myFunc();
 
 
+// function sum() {
+
+//     let nums = document.querySelectorAll(".num");
+//     let total = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+
+//         let value = Number(nums[i].innerText);
+//         total = total + value;
+
+//     }
+
+//     console.log(total);
+// }
+
+// sum();
 
 
+
+
+// function sum(){
+
+//     let nums = document.querySelectorAll(".num")
+//     let total = 0;
+//     for(let i = 0; i < nums.length; i++ ){
+
+//         let value = Number(nums[i].innerHTML);
+
+//         if(value % 2 != 0){
+
+//             total += value;
+//         }
+//     } 
+
+//     console.log(total);
+// }
+
+// sum()
+
+
+// function sum(){
+
+//     let nums = document.querySelectorAll(".num")
+//     for(let i = 0; i < nums.length; i++ ){
+
+//         nums[i].innerText = Number(nums[i].innerText);
+
+//         if(nums[i0] % 2 === 0){
+
+//          nums[i].innerText = nums[i].innerText * 2;
+     
+     
+//         }
+//     } 
+
+    
+// }
+
+// sum()
+
+
+// function greaterNumber() {
+
+//     let nums = document.querySelectorAll(".num");
+
+//     for (let value of nums) {
+
+//         let num = Number(value.innerText);
+
+//         if (num > 10) {
+//             value.innerText = "Big";
+//         } else {
+//             value.innerText = "Small";
+//         }
+//     }
+// }
+
+// greaterNumber();
+
+// function sum(){
+
+//     let nums = document.querySelectorAll(".num")
+
+//     for (let i = 0; i < nums.length; i++ ){
+
+//         let value = Number(nums[i].innerText);
+
+//          let main = nums[i].innerText = value +1;
+//          nums[i].innerText = main * main;
+
+
+
+//     }
+// }
+// sum()
+
+
+
+// function evenOdd(){
+
+//     let nums = document.querySelectorAll(".num");
+
+//     for( let i = 0; i < nums.length; i++){
+
+//         let value = Number(nums[i].innerText);
+
+        
+//         if (value % 2 === 0 ){ 
+
+//             nums[i].innerText = value + 2;
+    
+    
+//     } else {
+//         nums[i].innerText = value * value;
+//     }
+     
+
+// }
+    
+
+// }
+// evenOdd()
+
+
+
+
+
+function checkNumbers() {
+
+    let nums = document.querySelectorAll(".num");
+
+    for (let i = 0; i < nums.length; i++) {
+
+        let value = Number(nums[i].innerText);
+
+        if (value > 10) {
+            nums[i].innerText = "high";
+        } 
+        else if (value > 5 && value < 10)  {
+            nums[i].innerText = "Mid";
+        } 
+        else{ 
+            nums[i].innerText = "low";
+        }
+    }
+}
+
+checkNumbers();
